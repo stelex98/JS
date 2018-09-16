4ae787c5dde000ecfcfe1b034bab0f8a5e47c5ba
stelex98 <stelex98@mail.ru>
2018-09-16 15:51:01 +0300

    upadte_5

--- Task5/js/main.js
+++ Task5/js/main.js
@@ -1,96 +1,169 @@
-let add_span = document.getElementById('span_add');
-let cancel_btn = document.getElementById('cancel_button');
-let all_clear = document.getElementById('clear_all');
+let addSpan = document.getElementById('span_add');
+let cancelBtn = document.getElementById('cancel_button');
+let allClear = document.getElementById('clear_all');
 let close = document.getElementsByClassName("close");
 let i;
 let list = document.querySelector('ul');
+let yesButton = document.getElementById('yes_button');
+let noButton = document.getElementById('no_button');
+let input = document.getElementById('main_input');
+let inputValue = document.getElementById("main_input").value.trim();
 
-// Click on a close button to hide the current list item
-let a = function() {
-    [].forEach.call(document.querySelectorAll('#my_li'), function(e) { //удаление всех элементов
-        e.parentNode.removeChild(e);
+input.addEventListener('change', function() {
+    if (!input.value.trim()) {
+        addSpan.disabled = true;
+        addSpan.style.background = "#cccccc";
+    } else {
+        addSpan.style.background = " #d9d9d9";
+        addSpan.disabled = false;
+    }
+});
+
+window.onload = function() {
+    let arrayValues = localStorage.getItem('arrayInputValues');
+    arrayValues = JSON.parse(arrayValues) || []; // если null присваивает пустой массив
+    arrayValues.forEach((value) => {
+        newElement(value);
     });
 }
 
-all_clear.addEventListener('click', a);
-
-for (i = 0; i < close.length; i++) { // кнопка закрыть определенный элемент
-    close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
+let hide_ask_form = function() {
+    document.getElementById('form_ask').style.display = 'none';
+    document.getElementById('mask').style.display = 'none';
 }
+
+let checkAnswer = () => {
+    [].forEach.call(document.querySelectorAll('#my_li'), function(e) { //удаление всех элементов
+        e.parentNode.removeChild(e);
+    });
+    deleteAllValueToLocalStorage();
+    document.getElementById('form_ask').style.display = 'none';
+    document.getElementById('mask').style.display = 'none';
+}
+
+let a = () => {
+    document.getElementById('form_ask').style.display = 'block';
+    document.getElementById('mask').style.display = 'block';
+    yesButton.addEventListener('click', checkAnswer);
+}
+
+yesButton.addEventListener('click', checkAnswer);
+noButton.addEventListener('click', hide_ask_form);
+
+allClear.addEventListener('click', a);
 
 list.addEventListener('click', function(ev) { // редактирование
     if (ev.target.tagName === 'LI') {
         ev.target.classList.toggle('checked');
     } else
     if (ev.target.className === 'edit') {
-            let information = ev.target.parentElement.firstChild.textContent;
-            document.getElementById("information_task").value = information;
-            let btn = document.getElementById('save_button');
-            show_edit_from();
-            let handler = function() {
-                let text_from_area = document.getElementById('information_task').value;
-                ev.target.parentElement.firstChild.textContent = text_from_area;
-                btn.removeEventListener('click', handler);
-                cancel_function();
-            }
-            btn.addEventListener('click', handler);
+        let information = ev.target.parentElement.firstChild.textContent;
+        document.getElementById("information_task").value = information;
+        let btn = document.getElementById('save_button');
+        show_edit_from();
+        let handler = function() {
+            let textFromArea = document.getElementById('information_task').value;
+            ev.target.parentElement.firstChild.textContent = textFromArea;
+            let indexLi = [].indexOf.call(ev.target.parentElement.parentElement.children, ev.target.parentElement); // ищем индекс в ul нешго li
+            editValueToLocalStorage(indexLi, textFromArea);
+            btn.removeEventListener('click', handler);
+            cancel_function();
         }
+        btn.addEventListener('click', handler);
+    }
 }, false);
 
-let cancel_function = function() { //  hide widnow
+let cancel_function = function() {
     document.getElementById('form_edit').style.display = 'none';
     document.getElementById('mask').style.display = 'none';
 }
 
-let show_edit_from = function() { // show window
+let show_edit_from = function() {
     document.getElementById('form_edit').style.display = 'block';
     document.getElementById('mask').style.display = 'block';
 }
 
-cancel_btn.addEventListener('click', cancel_function);
+cancelBtn.addEventListener('click', cancel_function);
+
+function addValueToLocalStorage(inputValue) {
+    let arrayValues = localStorage.getItem('arrayInputValues');
+    arrayValues = JSON.parse(arrayValues) || []; // если null присваивает пустой массив
+    arrayValues.push(inputValue);
+    arrayValues = JSON.stringify(arrayValues); // преобразует наш массив в строку
+    localStorage.setItem('arrayInputValues', arrayValues);
+}
+
+function editValueToLocalStorage(index, value) { // редактирование
+    let arrayValues = localStorage.getItem('arrayInputValues');
+    arrayValues = JSON.parse(arrayValues) || [];
+    arrayValues[index] = value;
+    arrayValues = JSON.stringify(arrayValues);
+    localStorage.setItem('arrayInputValues', arrayValues);
+}
+
+function deleteValueToLocalStorage(index) {
+    let arrayValues = localStorage.getItem('arrayInputValues');
+    arrayValues = JSON.parse(arrayValues) || [];
+    arrayValues.splice(index, 1);
+    arrayValues = JSON.stringify(arrayValues);
+    localStorage.setItem('arrayInputValues', arrayValues);
+}
+
+function deleteAllValueToLocalStorage() {
+    localStorage.removeItem('arrayInputValues');
+}
+
+function preNewElement() {
+    var inputValue = document.getElementById("main_input").value;
+    if (inputValue) {
+        addValueToLocalStorage(inputValue);
+        newElement(inputValue);
+    } else {
+        alert("You must write something!");
+    }
+}
 
 //  Добавить элемент в список задач
-let newElement = function() {
+let newElement = function(inputValue) {
     var li = document.createElement("li");
     li.id = "my_li";
-    var inputValue = document.getElementById("main_input").value;
+
     var t = document.createTextNode(inputValue);
     li.appendChild(t);
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        document.getElementById("mainUL").appendChild(li);
-    }
+
+    document.getElementById("mainUL").appendChild(li);
     document.getElementById("main_input").value = "";
 
     var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7"); // символ крестика
+    var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
 
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
+    let closeLength = close.length;
+    for (i = 0; i < closeLength; i++) {
+        close[i].onclick = function(ev) {
+            let myUl = document.getElementById('mainUL');
+            let indexLi = [].indexOf.call(myUl.children, this.parentElement); // ищем индекс в ul нешго li
+            deleteValueToLocalStorage(indexLi);
+            this.parentElement.remove();
         }
     }
 
-    var edit_ = document.createElement("SPAN");
-    var txt_edit = document.createTextNode("Edit");
-    edit_.className = "edit";
-    edit_.appendChild(txt_edit);
-    li.appendChild(edit_);
+    var edit = document.createElement("SPAN");
+    var txtEdit = document.createTextNode("\u270E");
+    edit.className = "edit";
+    edit.appendChild(txtEdit);
+    li.appendChild(edit);
 
-    for (i = 0; i < edit_.length; i++) {
-        edit_[i].onclick = function() {
+    let editLength = edit.length;
+    for (i = 0; i < editLength; i++) {
+        edit[i].onclick = function() {
+
             var div = this.parentElement;
             console.log(div);
         }
     }
 }
 
-add_span.addEventListener('click', newElement); // вызов ф-ии при клике "Добавить эл-т"
+addSpan.addEventListener('click', preNewElement);
